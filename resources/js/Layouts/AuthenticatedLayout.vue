e6486b328315f44b7dd0a9354491ae643b40abe2
AngelLaines <angelmoreno755@gmail.com>
2023-10-25 15:03:15 -0700

    first commit

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import SearchForm from '@/Components/app/SearchForm.vue';
+import Navigation from '@/Components/app/Navigation.vue';
+import UserSettingsDropdown from '@/Components/app/UserSettingsDropdown.vue';
+</script>
+
+<template>
+    <div 
+        class="h-screen bg-gray-50 flex w-full gap-4"
+    >
+        <Navigation/>
+
+        <main
+            class="flex flex-col flex-1 px-4 overflow-hidden"
+        >
+            <div
+                class="flex items-center justify-between w-full"
+            >
+                <SearchForm/>
+                <UserSettingsDropdown/>
+            </div>
+            <div
+                class="flex-1 flex flex-col overflow-hidden"
+            >
+                <slot></slot>
+            </div>
+        </main>
+    </div>    
+</template>
